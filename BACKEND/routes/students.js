816a466913f93ee44ce69a571c1767a65fce541e
anupamamaheepala <127319824+anupamamaheepala@users.noreply.github.com>
2024-02-02 11:23:13 +0530

    students comiited

--- BACKEND/routes/students.js
+++ BACKEND/routes/students.js
@@ -33,6 +33,41 @@ router.route("/").get((req,res)=>{
 router.route("/update/:id").put(async (req,res) =>{
     let userId = req.params.id;
     const {name, age, gender} = req.body;
+
+    const updateStudent = {
+        name,
+        age,
+        gender
+    }
+
+    const update = await Student.findByAndUpdate(userId, updateStudent).then(() =>{
+        res.status(200).send({status: "User updated", user: update})
+    }).catch((err) =>{
+        console.log(err);
+        res.status(500).send({status: "Error with updating data", error: err.message});
+    })
+
+})
+
+router.route("/delete/:id").delete(async (req,res) =>{
+    let userId = req.params.id;
+
+    await Student.findByAndDelete(userId).then(() =>{
+        res.status(200).send({status: "User deleted"});
+    }).catch((err) => {
+        console.log(err.massage);
+        res.status(500).send({status: "Error with delete user", error: err.massage});
+    })
+})
+
+router.route("/get/id").get(async (req,res) =>{
+    let userId = req.params.id;
+    const user = await Student.findById(userId).then(() =>{
+        res.status(200).send({status : "User fetched"})
+    }).catch(() => {
+        console.log(err.massage);
+        res.status(500).send({status: "Error with get user", error: err.massage});
+    })
 })
 
 module.exports = router;
