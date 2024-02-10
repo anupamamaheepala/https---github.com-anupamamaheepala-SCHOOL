0da5024e20321c59fc58b6126ae2c79b7352509e
anupamamaheepala <127319824+anupamamaheepala@users.noreply.github.com>
2024-02-10 18:52:11 +0530

    1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import CounterClass from './components/CounterClass';
-import CounterFunction from './components/CounterFunction';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React</h1>
-      <hr></hr>
-      <CounterClass/>
-      <CounterFunction/>
+    <div>
+      <Header/>
     </div>
   );
 }
